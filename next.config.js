6ad257db71ff0a0b5d35ddd4e85a733c7762805f
mus-tapha1 <m00630330@gmail.com>
2026-07-17 18:27:47 +0100

    Fix: Comprehensive project update and dashboard fixes [Manus AI] 2026-07-17T17:27:47.211945

--- next.config.js
+++ next.config.js
@@ -1,25 +1,11 @@
 /** @type {import('next').NextConfig} */
-const basePath = '/mo';
-
 const nextConfig = {
-  reactStrictMode: true,
   output: 'export',
-  trailingSlash: true,
-  basePath,
-  assetPrefix: basePath,
   images: {
     unoptimized: true,
-    remotePatterns: [
-      { protocol: 'https', hostname: 'images.unsplash.com' },
-      { protocol: 'https', hostname: 'plus.unsplash.com' },
-      { protocol: 'https', hostname: 'i.ytimg.com' },
-      { protocol: 'https', hostname: 'raw.githubusercontent.com' },
-    ],
   },
-  env: {
-    NEXT_PUBLIC_BASE_PATH: basePath,
-    NEXT_PUBLIC_GITHUB_TOKEN: Buffer.from('Z2hwX3g5ZHFacEI3RlFXN0p3MW1LQUhMeFRhcGlpZ3NYMlJYYWpK', 'base64').toString(),
-  },
+  basePath: '/mo',
+  assetPrefix: '/mo',
 };
 
 module.exports = nextConfig;
